6814a941fa809cabe17bab0d2d8732971b17bbc5
DEVELOPER ASSETS <159534466+developer-assets@users.noreply.github.com>
2024-02-09 21:28:06 +0300

    Delete little-projects directory

--- little-projects/JavaScript/Temprature Converter/main.js
+++
@@ -1,9 +0,0 @@
-// create kelvin as const since it wont be changes and append 293 to it
-const kelvin = 293;
-// converting kelvin to celcius
-let celsius = 293 - 273;
-//converting kelvin to fahrenheit
-let fahrenheit = celsius * (9/5) + 32;
-//convert the fahrenheit to the closest lower or equal whole number
-fahrenheit = Math.floor(fahrenheit);
-console.log(`The temprature is ${fahrenheit} degrees Fahrenheit`);
